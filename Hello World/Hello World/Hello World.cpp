ae55af7252ccf29cf3fb65b7dafbc472c8d388bf
CryptoxGuy <54878560+CryptoxGuy@users.noreply.github.com>
2019-12-12 14:16:02 -0500

    Hello-World in C++

---
+++ Hello World/Hello World/Hello World.cpp
@@ -0,0 +1,14 @@
+// Basic program everyone has done 1,000 times. Displays "Hello World" on screen.
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello World" << endl;
+	cout << "Are you tired of seeing these yet?" << endl;
+
+	system("pause");
+	return 0;
+}
